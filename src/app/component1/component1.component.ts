b4e24b0ff5ff5b80e531f0992e95a4056e25508a
vijayr2000 <vijaykrdsm@gmail.com>
2022-09-23 14:23:36 +0530

    adding some code

--- src/app/component1/component1.component.ts
+++ src/app/component1/component1.component.ts
@@ -6,12 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./component1.component.scss']
 })
 export class Component1Component implements OnInit {
-
-  constructor() { }
+list:any;
+  constructor() {  }
 
   ngOnInit(): void {
-
-  }
+  this.list=[
+    {
   
+    id:1,
+    Name:"vijay",
+    age:22,
+    city:"kumbakonam",
+     state:"Tamilnadu"
+    },
 
+    {id:2,
+    Name:"siva",
+    age:20,
+    city:"dsm",
+     state:"Tamilnadu"},
+
+    {id:3,
+    Name:"vimal",
+    age:22,
+    city:"kumbakonam",
+     state:"Tamilnadu"},
+
+    {id:4,
+    Name:"karthi",
+    age:22,
+    city:"cuddalore",
+     state:"Tamilnadu"},
+
+    {id:5,
+    Name:"yc",
+    age:22,
+    city:"salam",
+     state:"Tamilnadu"},
+
+    {id:6,
+    Name:"kamal",
+    age:22,
+    city:"chennai",
+     state:"Tamilnadu"
+
+    
+  }]
+
+}
 }
